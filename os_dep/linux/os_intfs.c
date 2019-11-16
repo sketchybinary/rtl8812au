fa5503c5895369e716927f6853e2ed756261e179
sketchybinary <will@sketchybinary.com>
2019-11-16 02:55:26 +0000

    These struct members do not exist in the Jetson Nano kernel sources.  Maybe we can try to narrow down what the kernel these were introduced in.

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -1589,9 +1589,12 @@ struct net_device *rtw_init_netdev(_adapter *old_padapter)
 	if (!pnetdev)
 		return NULL;
 
+#if (LINUX_VERSION_CODE > KERNEL_VERSION(4, 9, 140))
 	pnetdev->min_mtu = WLAN_MIN_ETHFRM_LEN;
-	pnetdev->mtu = WLAN_DATA_MAXLEN;
 	pnetdev->max_mtu = WLAN_DATA_MAXLEN;
+#endif
+
+	pnetdev->mtu = WLAN_DATA_MAXLEN;
 
 	pnetdev->dev.type = &wlan_type;
 	padapter = rtw_netdev_priv(pnetdev);
